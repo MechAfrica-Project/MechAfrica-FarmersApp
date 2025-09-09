3131f6f347982c392a27db5bc1a133362f4f41a8
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-09-09 13:37:51 +0000

    still on setup

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      
+      className="bg-green-800 h-full"
+    >
+      <Text className="">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
