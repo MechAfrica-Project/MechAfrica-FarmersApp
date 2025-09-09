d79601b76f03fb88cabe7641e31963fcbb5cc762
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-09-09 15:14:31 +0000

    left with yellow background for the splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,29 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { View } from "react-native";
 import "./globals.css";
 
+// Keep splash screen visible until we hide it
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    MulishRegular: require("../assets/fonts/Mulish-Regular.ttf"),
+    MulishBold: require("../assets/fonts/Mulish-Bold.ttf"),
+    MulishSemiBold: require("../assets/fonts/Mulish-SemiBold.ttf"),
+  });
+
+  // Hide splash when fonts are loaded
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // Show nothing until fonts load
+  if (!fontsLoaded) return <View style={{ flex: 1 }} />;
+
   return <Stack />;
 }
