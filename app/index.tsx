25cee3c14ef1ebdd850bcc1ea8849a641eb538e1
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-09-09 16:35:48 +0000

    test

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      
-      className="h-full"
-    >
-      <Text className="">Edit app/index.tsx to edit this screen.</Text>
+    <View className="h-full">
+      <Text className="text-red-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
