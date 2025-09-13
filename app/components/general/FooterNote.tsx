24cdaf71cc07a696dbff44c619e02c5c334a0eb9
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-09-13 04:01:28 +0000

    updated footer note and back btn

--- app/components/general/FooterNote.tsx
+++ app/components/general/FooterNote.tsx
@@ -1,12 +1,22 @@
+// components/FooterNote.tsx
+import React from "react";
 import { View, Text } from "react-native";
 
-export default function FooterNote() {
+type Props = {
+  /** When true the text is shown. Defaults to false (hidden). */
+  showText?: boolean;
+  message?: string;
+};
+
+export default function FooterNote({ showText = false, message }: Props) {
   return (
     <View className="py-4 h-[8rem] bg-light-yellow flex flex-col pb-12 justify-center align-middle w-full">
-      <Text className="text-gray-color font-mulish text-center">
-        By using MechAfrica, you agree to the{"\n"}
-        Terms and Privacy Policy.
-      </Text>
+      {showText ? (
+        <Text className="text-gray-color font-mulish text-center">
+          {message ??
+            "By using MechAfrica, you agree to the\nTerms and Privacy Policy."}
+        </Text>
+      ) : null}
     </View>
   );
 }
