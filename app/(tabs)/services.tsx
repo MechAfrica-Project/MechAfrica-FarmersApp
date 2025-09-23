5fc06b0d13a58c05a7f5c3df3792e24471ebd070
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-09-23 12:46:43 +0000

    completed serviceRequestInfo section

--- app/(tabs)/services.tsx
+++ app/(tabs)/services.tsx
@@ -1,12 +1,13 @@
-import { ScrollView, Text } from "react-native";
+import { ScrollView } from "react-native";
 import React from "react";
 import MultiToneBackground from "../components/general/MultiToneBackground";
+import ServiceRequestSection from "../components/service/ServiceRequestSection";
 
 const Services = () => {
   return (
     <MultiToneBackground mainColor="#FDFFE0">
       <ScrollView className="pt-[4rem]">
-        <Text className="text-center text-lg font-bold ">Service tab</Text>
+        <ServiceRequestSection />
       </ScrollView>
     </MultiToneBackground>
   );
---
+++ app/components/service/ServiceRequestSection.tsx
@@ -0,0 +1,31 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import { useOnboardingStore } from "@/stores/onboardingStore";
+
+const ServiceRequestSection = () => {
+  const { data } = useOnboardingStore();
+  return (
+    <View className="relative flex-row bg-primary-green overflow rounded-3xl mt-6 mx-4 h-[10rem] p-6">
+      {/* Left section (text) */}
+      <View className="flex-1 justify-between bg-primary-green rounded-3xl">
+        <View className="pr-[10.2rem]">
+          <Text className="text-accent-yellow font-mulish font-bold text-base mb-2">
+            {data.personalInfo.name} what service do you need?
+          </Text>
+          <Text className="text-white font-mulish text-sm leading-5">
+            MechAfrica will connect you{"\n"} to a service provider now.
+          </Text>
+        </View>
+      </View>
+
+      {/* Right section (farmer + maize) */}
+      <Image
+        source={images.farmer}
+        className="absolute bottom-0 right-0 z-10 rounded-3xl"
+      />
+    </View>
+  );
+};
+
+export default ServiceRequestSection;
