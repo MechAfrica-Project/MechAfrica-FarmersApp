88264a573873538a5152b22a5b98fdecf9a6085c
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-11-05 11:32:31 +0000

    added request card details reusable component and a progressbar

---
+++ app/components/requests/reusable/MessageFromFarmer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View } from "react-native";
+import AudioMessage from "./message/AudioMessage";
+import TextMessage from "./message/TextMessage";
+
+interface MessageFromFarmerProps {
+  request: {
+    messageFromFarmer?: string;
+    voiceNoteUrl?: string;
+  };
+}
+
+const MessageFromFarmer = ({ request }: MessageFromFarmerProps) => {
+  return (
+    <View>
+      <TextMessage message={request?.messageFromFarmer} />
+      <AudioMessage voiceNoteUrl={request?.voiceNoteUrl} />
+    </View>
+  );
+};
+
+export default MessageFromFarmer;
