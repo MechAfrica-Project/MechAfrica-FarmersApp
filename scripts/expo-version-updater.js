4d90094c4b467317014db14ce61addfd36669a8f
ProvLeon <providence.leonard@gmail.com>
2026-07-25 02:54:28 +0000

    chore: bump version to 1.7.0 and automate Android versionCode incrementing in configuration and updater script

--- scripts/expo-version-updater.js
+++ scripts/expo-version-updater.js
@@ -5,5 +5,12 @@ module.exports.readVersion = function readVersion(contents) {
 module.exports.writeVersion = function writeVersion(contents, version) {
   const json = JSON.parse(contents);
   json.expo.version = version;
+  
+  // Auto-increment Android versionCode
+  if (!json.expo.android) {
+    json.expo.android = {};
+  }
+  json.expo.android.versionCode = (json.expo.android.versionCode || 1) + 1;
+  
   return `${JSON.stringify(json, null, 2)}\n`;
 };
